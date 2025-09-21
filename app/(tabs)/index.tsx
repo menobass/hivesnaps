b71aa97e803353cb4f200b0c4a520996b90acc23
menobass <menoecua@gmail.com>
2025-09-21 15:26:12 -0500

    fix: use standard React Native Text/View components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,9 +11,11 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   ActivityIndicator,
+  Text,
+  View,
 } from 'react-native';
 import { useState, useEffect } from 'react';
-import { Text, View } from '@/components/Themed';
+//import { Text, View } from '@/components/Themed';
 import { useRouter } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 import { Client, PrivateKey } from '@hiveio/dhive';
