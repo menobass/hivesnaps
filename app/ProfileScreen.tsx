fd076240a546c9376aa032a354ca29679d005f02
Jose A. Mena <josemena79@yahoo.com>
2025-09-23 09:58:44 -0400

    create an index.tsx at the top level

--- app/ProfileScreen.tsx
+++ app/ProfileScreen.tsx
@@ -804,7 +804,7 @@ const ProfileScreen = () => {
                   style={[styles.logoutButton, { backgroundColor: '#E74C3C' }]}
                   onPress={async () => {
                     await handleLogout();
-                    router.replace('/(tabs)');
+                    router.replace('/LoginScreen');
                   }}
                 >
                   <FontAwesome name='sign-out' size={18} color='#fff' />
